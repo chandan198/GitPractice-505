ae6ff509face7297f303b7d5766c3af50ec0a53f
chandan <chandansinghcsebanglore@gmail.com>
2021-08-13 20:45:02 +0530

    developer 1 changes related to 2nd story

--- src/main/java/com/org/service/HelloJava.java
+++ src/main/java/com/org/service/HelloJava.java
@@ -10,6 +10,8 @@ public class HelloJava {
 	public HelloJava() {
 		System.out.println("Hello Java :: constructor");
 		System.out.println(“Developer 1 did the changes");
+		System.out.println(“Developer 1 did changes related to 2nd story");
+
 
 	}
 	
